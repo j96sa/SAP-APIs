602487e9550e728f47ac1093c887fcad4df50aac
Josue Paulo Antuan <josue96sa@gmail.com>
2021-08-20 17:34:10 +0200

    haciendo el responsive 17:34

--- app/components/Com_Home.js
+++ app/components/Com_Home.js
@@ -1,3 +1,4 @@
+import Observer from "../helpers/Observer.js";
 import { H_SectionActorsSingers, H_SectionActorsSingers_Com } from "./H_Section-Actors&Singers.js";
 import { H_Section_Art, H_Section_Art_Com } from "./H_Section-Art.js";
 import {H_Section_Film, H_Section_Film_Com} from "./H_Section-Film.js";
@@ -11,4 +12,7 @@ export function Com_Home() {
     H_Section_Film();
     H_SectionActorsSingers();
     H_Section_Art();
+    
+    const $article = document.querySelectorAll(".home-article");
+    Observer($article);
 };
--- app/helpers/Observer.js
+++ app/helpers/Observer.js
@@ -1,20 +1,15 @@
-export default function effect(obs){
-    const d = document,    
-
+export default function Observer(obs){       
+    
     const cb =(entries)=>{        
         entries.forEach(entry=>{
             if(entry.isIntersecting){
-                console.log(entry.target.id);
-                entry.target.style.backgroundColor = `#fd0`;
                 entry.target.classList.add("active");
             }else{
-                console.log(`${entry.target.id}is not intersecting`);
-                entry.target.style.backgroundColor = `#f30`;
                 entry.target.classList.remove("active");
             };
         });
     };    
 
-    const par = new IntersectionObserver(cb,{threshold:.2});
+    const par = new IntersectionObserver(cb,{threshold:.1});
     obs.forEach(e=>par.observe(e));
 };
