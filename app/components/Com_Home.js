4ce6558f469573f94813859ccc7c088a3d24f742
Josue Paulo Antuan <josue96sa@gmail.com>
2021-08-23 12:25:54 +0200

    intoduje el component loader, efecto para las imagenes de la galeria en modo desktop home-ok 23/08---12:25

--- app/components/Com_Home.js
+++ app/components/Com_Home.js
@@ -3,16 +3,16 @@ import { H_SectionActorsSingers, H_SectionActorsSingers_Com } from "./H_Section-
 import { H_Section_Art, H_Section_Art_Com } from "./H_Section-Art.js";
 import {H_Section_Film, H_Section_Film_Com} from "./H_Section-Film.js";
 
-export function Com_Home() {   
+export async function Com_Home() {   
     const $main = document.getElementById("main");
 
     $main.innerHTML = H_Section_Film_Com();
     $main.innerHTML += H_SectionActorsSingers_Com();
-    $main.innerHTML += H_Section_Art_Com();
-    H_Section_Film();
+    $main.innerHTML += H_Section_Art_Com();        
+    H_Section_Film();    
     H_SectionActorsSingers();
     H_Section_Art();
-    
+
     const $article = document.querySelectorAll(".home-article");
     Observer($article);
 };
