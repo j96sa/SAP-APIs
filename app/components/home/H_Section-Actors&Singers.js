15be1205701a1760e830017a7d04e69f6b2d93f6
Josue Paulo Antuan <josue96sa@gmail.com>
2022-02-04 21:36:34 +0100

    refactor(home): home refactorization done

--- app/components/home/H_Section-Actors&Singers.js
+++ app/components/home/H_Section-Actors&Singers.js
@@ -2,25 +2,15 @@ export function H_SectionActorsSingers_Com() {
     return `
         <article id="article-actors" class="article-actors home-article">
             <section><h2 class="subtitle">You can also search for information about your favorite <span>actors</span> and <span>singers,</span> their <span>biography, discography,</span> and <span>greatest hits</span>.</h2></section>
-            <div class="artist-img home-img"></div>
+            <div class="artist-img home-img">
+                <section>
+                    <img src="https://i1.wp.com/revistadiners.com.co/wp-content/uploads/2019/06/michael_jackson_1200x800.jpg?fit=1200%2C800&ssl=1&resize=1280%2C720" alt="img"/>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Matthew_McConaughey_2019_%2848648344772%29.jpg/250px-Matthew_McConaughey_2019_%2848648344772%29.jpg" alt="img"/>
+                    <img src="https://pyxis.nymag.com/v1/imgs/b46/bf9/e4dc0519c559430f2c667f842d3249739d-beyonce-1.rvertical.w1200.jpg" alt="img"/>
+                    <img src="https://m.media-amazon.com/images/I/41iGW7dsb7L._AC_.jpg" alt="img"/>
+                </section>  
+            </div>
         </article>  
     `;
-};
-
-export function H_SectionActorsSingers(){
-    const d = document,
-    $fragment = d.createDocumentFragment(),
-    IMG = ["https://i1.wp.com/revistadiners.com.co/wp-content/uploads/2019/06/michael_jackson_1200x800.jpg?fit=1200%2C800&ssl=1&resize=1280%2C720","https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Matthew_McConaughey_2019_%2848648344772%29.jpg/250px-Matthew_McConaughey_2019_%2848648344772%29.jpg","https://m.media-amazon.com/images/I/41iGW7dsb7L._AC_.jpg","https://pyxis.nymag.com/v1/imgs/b46/bf9/e4dc0519c559430f2c667f842d3249739d-beyonce-1.rvertical.w1200.jpg"];
-    
-    IMG.forEach(e=>{
-      let img = d.createElement("img");
-      img.src = e;
-      img.alt = "img";        
-      $fragment.appendChild(img);
-    });
-
-    const $section = d.createElement("section");
-    $section.appendChild($fragment);
-    d.querySelector(".article-actors .artist-img").appendChild($section);
 };
 
